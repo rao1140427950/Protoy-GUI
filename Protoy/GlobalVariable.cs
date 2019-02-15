db1e48f8306ea7e08d1f7aa749d67bff578ac26f
sj rao <raoshijie@qq.com>
2019-02-15 14:17:11 +0800

    Add navbar

---
+++ Protoy/GlobalVariable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protoy
+{
+    public static class GlobalVariable
+    {
+        public static string C51_path = "";
+        public static string C51_diskname = "";
+        public static string C51_folder = "";
+        public static string port = "";
+    }
+}
--- Protoy/MainWindow.xaml.cs
+++ Protoy/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -23,6 +24,18 @@ namespace Protoy
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Nav_Settings_C51_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog openFileDialog = new FolderBrowserDialog();
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                GlobalVariable.C51_path = openFileDialog.SelectedPath;
+                GlobalVariable.C51_diskname = GlobalVariable.C51_path.Split(':')[0];
+                GlobalVariable.C51_folder = GlobalVariable.C51_path.Split(':')[1].Remove(0, 1);
+                System.Windows.MessageBox.Show(GlobalVariable.C51_diskname+'\n'+GlobalVariable.C51_folder);
+            }
         }
     }
 }
